59736d3f52b75ce6754067abf49c82f0a23c3617
Robb Shecter <robb@public.law>
2025-08-28 00:25:36 -0600

    docs

--- lib/schema_dot_org/postal_address.rb
+++ lib/schema_dot_org/postal_address.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 #
-# Model the Schema.org `Place > ContactPoint > PostalAddress`.
+# Model the Schema.org `Thing > Intangible > StructuredValue > ContactPoint > PostalAddress`.
 # See https://schema.org/PostalAddress
 #
 module SchemaDotOrg
@@ -13,7 +13,8 @@ module SchemaDotOrg
     # and which is in the region. For example, Mountain View.
     validated_attr :addressLocality, type: String, allow_nil: true
 
-    # AddressRegion: The region. For example, CA.
+    # AddressRegion: The region in which the locality is, and which is in the country.
+    # For example, California or another appropriate first-level Administrative division.
     validated_attr :addressRegion,   type: String, allow_nil: true
 
     # PostalCode: The postal code. For example, 94043.
