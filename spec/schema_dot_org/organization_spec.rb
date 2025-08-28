cd9a8f30e3e0c953ab2101d893498240a52f6977
Robb Shecter <robb@public.law>
2025-08-28 15:26:38 -0600

    comments

--- spec/schema_dot_org/organization_spec.rb
+++ spec/schema_dot_org/organization_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe SchemaDotOrg::Organization do
     it 'will not create with an unknown attribute' do
       expect do
         SchemaDotOrg::Organization.new(
-          snack_time: 'today',
+          snack_time: 'today', # !!!
           name: 'Public.Law',
           founder: SchemaDotOrg::Person.new(name: 'Robb Shecter'),
           founding_date: Date.new(2009, 3, 6),
@@ -61,17 +61,17 @@ RSpec.describe SchemaDotOrg::Organization do
           founding_date: Date.new(2009, 3, 6),
           founding_location: SchemaDotOrg::Place.new(address: 'Portland, OR'),
           email: 'say_hi@public.law',
-          url: 'https://www.public.law',
+          logo: 'https://www.public.law/favicon-196x196.png',
           same_as: [
             'https://twitter.com/law_is_code',
             'https://www.facebook.com/PublicDotLaw'
           ]
         )
-      end.to raise_error(ArgumentError, /Logo/)
+      end.to raise_error(ArgumentError, /Url/)
     end
 
 
-    it 'is invalid without a logo' do
+    it 'is invalid without a name' do
       expect do
         SchemaDotOrg::Organization.new(
           founder: SchemaDotOrg::Person.new(name: 'Robb Shecter'),
